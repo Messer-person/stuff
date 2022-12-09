9fb2638ddd2941dd14122ef4d6a11fe86cff6ece
Messer-person <tomgmisiek@gmail.com>
2022-12-09 12:14:52 +0100

    putting play loop into a function, and slight code refracting

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 # Generates a 4 digit number
@@ -19,27 +20,27 @@ def string_cutter(string_para):
 
 # Checks for a correct numbers in correct place in a sequence.
 def cows(correct_pin, guess_para):
-    cows = 0
+    cows_count = 0
     x = 0
-    for numb in guess_para:
+    for _ in guess_para:
         if correct_pin[x] == guess_para[x]:
-            cows += 1
+            cows_count += 1
             x += 1
         else:
             x += 1
-    return cows
+    return cows_count
 
 
-# Checks for a correct numbers in a incorrect place in a sequence.
+# Checks for a correct numbers in an incorrect place in a sequence.
 def bulls(correct_pin, guess_para, cow_para):
     guess_shattered = string_cutter(guess_para)
     pin_shattered = string_cutter(correct_pin)
-    bull = 0
+    bulls_count = 0
     for number in guess_shattered:
         if number in pin_shattered:
-            bull += 1
+            bulls_count += 1
             pin_shattered.remove(number)
-    return bull - cow_para
+    return bulls_count - cow_para
 
 
 pin = generator()
@@ -51,24 +52,42 @@ for every number guessed correctly in the right place in the sequence it's a cow
 for every number guessed correctly in the wrong place it's a bull.
 good luck!""")
 
+
 # Game loop.
-attempt = 0
+def play():
+    attempt = 0
+    while True:
+        guess = (input("give me a number: "))
+        attempt += 1
+        if pin == guess:
+            print(f"""correct, the number was {pin}!
+             It took you {attempt} tries! 
+             Thanks for playing!""")
+            break
+        elif len(guess) != 4:
+            print("Enter 4 digits.")
+            pass
+        elif guess.upper() == "QUIT":
+            print("Thank you for playing, bye.")
+            break
+        else:
+            cow = cows(pin, guess)
+            bull = bulls(pin, guess, cow)
+            print(f"cows: {cow}, bulls: {bull}")
+            pass
+
+
+play()
+
 while True:
-    guess = (input("give me a number: "))
-    attempt += 1
-    if pin == guess:
-        print(f"""correct, the number was {pin}!
-         It took you {attempt} tries! 
-         Thanks for playing!""")
-        break
-    elif len(guess) != 4:
-        print("Enter 4 digits.")
-        pass
-    elif guess.upper() == "QUIT":
-        print("Thank you for plaing, bye.")
+    continuation = input("""Would you like to play again or quit the app? 
+    Play again/ quit app "P"/"Q" :""")
+    if continuation.upper() == "P":
+        play()
+    elif continuation.upper() == "Q":
+        print("Thank you for playing")
+        time.sleep(2)
         break
     else:
-        cow = cows(pin, guess)
-        bull = bulls(pin, guess, cow)
-        print(f"cows: {cow}, bulls: {bull}")
+        print("Wrong command, please try again: ")
         pass
